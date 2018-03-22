061128c6985967e43f517ddec6d9ff327a1d0873
Michelle Olivieri <michelleo72@gmail.com>
2018-03-22 15:45:21 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -99,10 +99,10 @@ def changing_alan
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
         },
-        :alan_kay => [{
+        :alan_kay => {
           :known_for => "Object Orientation",
           :languages => ["Smalltalk", "LISP"]
-        }],
+        },
         :dennis_ritchie => {
           :known_for => "Unix",
           :languages => ["C"]
