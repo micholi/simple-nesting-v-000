2101044151fc113bd064a1e6537d72e174061e60
Michelle Olivieri <michelleo72@gmail.com>
2018-03-22 14:54:16 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -87,7 +87,8 @@ def adding_matz
         }
      }
 
-programmer_hash[:yukihiro_matsumoto]
+programmer_hash[:yukihiro_matsumoto] = {[:known_for] = "Ruby", [:languages] = ["LISP, "C"]}
+
 
 # :yukihiro_matsumoto => {
 #   :known_for => "Ruby",
